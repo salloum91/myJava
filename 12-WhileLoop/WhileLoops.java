91c9cf7be8d026edb7546276f957fa353e58afb1
salloum91 <aboward70@gmail.com>
2025-05-31 09:32:51 -0700

    added Whileloops.

---
+++ 12-WhileLoop/WhileLoops.java
@@ -0,0 +1,23 @@
+public class WhileLoops {
+
+    public static void main(String[]args){
+
+        int i = 1 ;
+
+
+        while(i <= 5 ){
+            System.out.println("hi "+ i);
+            int j = 1;
+            while (j<=3){
+
+                System.out.println("hello");
+                j++;
+            }
+
+
+            i++;
+        }
+
+    }
+}
+//for every hi going to print 3 hello out
