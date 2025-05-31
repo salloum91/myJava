229d697ac0020a9fccff2c962053d1407dc68937
salloum91 <aboward70@gmail.com>
2025-05-31 09:45:07 -0700

    added DoWhileloops.

---
+++ 13-DoWhileLoop/DoWhileLoop.java
@@ -0,0 +1,15 @@
+public class DoWhileLoop {
+    public static void main(String[] args){
+
+        // used when a situation the condition if false and run 1 time
+
+        int i = 5;
+
+        do{
+            System.out.println("hello "+1);
+            i++;
+        }while(i <=4);
+
+    }
+
+}
